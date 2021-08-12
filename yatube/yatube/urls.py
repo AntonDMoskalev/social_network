24f0a5acc9b04ee9395524889c14550e8801f1bf
Антон <a.d.moskalev@yandex.ru>
2021-08-12 12:39:10 +1000

    Commit 5:12

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,4 +1,3 @@
-from posts.views import page_not_found, server_error # noqa
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import handler404, handler500
